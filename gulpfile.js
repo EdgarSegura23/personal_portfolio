3b768f400f8b7df9de65c87511a5f58b99d01453
Edgar Segura <edgarsegura@MacBook-Air-de-Eric.local>
2024-12-15 13:30:39 -0600

    Add Boxicons and js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 //* Module Import
-import { src, dest, watch } from 'gulp';
-
+import { src, dest, watch, series } from 'gulp';
 import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
+import terser from 'gulp-terser';
 
 //* Sass compiler configuration
 const sass = gulpSass(dartSass);
 
 //TODO: Function Definition: 
+
+//* Copy Boxicons CSS
+export function copyBoxiconsCss(done) {
+    src("node_modules/boxicons/css/boxicons.min.css")
+        .pipe( dest('build/css') );
+    done();
+}
 
 //* CSS Function definition
 export function css( done ) {
@@ -17,7 +24,25 @@ export function css( done ) {
     done()
 }
 
+//* Copy Boxicons js
+export function copyBoxiconsJs(done) {
+    src("node_modules/boxicons/dist/boxicons.js")
+        .pipe( terser() )
+        .pipe(dest("build/js"));
+    done();
+}
+
+export function js(done) {
+    src('src/js/app.js')
+        .pipe( terser() )
+        .pipe( dest('build/js') )
+    done();
+}
+
 //* dev Function definition
 export function dev() {
     watch('src/scss/**/*.scss', css)
+    watch('src/js/**/*.js', js)
 }
+
+export default series(copyBoxiconsCss, css, copyBoxiconsJs, js, dev);
